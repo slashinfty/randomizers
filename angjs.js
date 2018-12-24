6f93f3028a3e68b9e4968cbb7e05c234c4eb901d
slashinfty <matt.braddock@gmail.com>
2018-12-24 09:53:40 -0500

    trimming table width

--- angjs.js
+++ angjs.js
@@ -1,6 +1,6 @@
 var randoApp = angular.module('randoApp', []);
 randoApp.controller('RandoCtrl', function ($scope, $http) {
-    $http.get('randomizers.json').success(function(data) {
+    $http.get('https://api.myjson.com/bins/15epbg').success(function(data) {
         $scope.randomizers = data;
     });
     $scope.sortField = 'name';
