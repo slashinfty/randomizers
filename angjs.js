40e793a8dc2e38c37724dd442cbf6ae8d8c07c1e
Matt Braddock <matt.braddock@gmail.com>
2018-12-26 11:33:20 -0500

    bad json ref

--- angjs.js
+++ angjs.js
@@ -1,6 +1,6 @@
 var randoApp = angular.module('randoApp', []);
 randoApp.controller('RandoCtrl', function ($scope, $http) {
-    $http.get('https://api.myjson.com/bins/s3pho').success(function(data) {//randomizers.json
+    $http.get('randomizers.json').success(function(data) {
         $scope.randomizers = data;
     });
     $scope.sortField = 'name';
