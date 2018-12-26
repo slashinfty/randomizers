89bc5f3bade8530208228b7b7668ab27281502fd
Matt Braddock <matt.braddock@gmail.com>
2018-12-26 11:32:34 -0500

    final mobile update; reverse sorting

--- angjs.js
+++ angjs.js
@@ -1,7 +1,8 @@
 var randoApp = angular.module('randoApp', []);
 randoApp.controller('RandoCtrl', function ($scope, $http) {
-    $http.get('randomizers.json').success(function(data) {
+    $http.get('https://api.myjson.com/bins/s3pho').success(function(data) {//randomizers.json
         $scope.randomizers = data;
     });
     $scope.sortField = 'name';
+    $scope.reverse = false;
 });
